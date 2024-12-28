e6089e061fd593a7d7fab57c0bb821d96fb9ea9e
renjii71 <taras.kozak08@gmail.com>
2024-12-28 22:19:54 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,40 @@
+const body = document.body
+const switchThemeBtn = document.getElementById('switchTheme-button')
+
+const burgerBtn = document.querySelector('.burger__button')
+const burgerWindow = document.querySelector('.burger__window')
+switchThemeBtn.addEventListener('click', () => {
+    if(switchThemeBtn.classList.contains('fa-solid')) {
+        switchThemeBtn.classList.remove('fa-solid')
+        switchThemeBtn.classList.add('fa-regular')
+    } else {
+        switchThemeBtn.classList.remove('fa-regular')
+        switchThemeBtn.classList.add('fa-solid')
+    }
+    body.classList.toggle('dark-theme')
+    localStorage.setItem('theme', body.classList.contains('dark-theme')? 'dark' : 'light')
+})
+
+window.addEventListener('load', () => {
+    const savedTheme = localStorage.getItem('theme')
+    if(savedTheme === 'dark') {
+        body.classList.add('dark-theme')
+        switchThemeBtn.classList.remove('fa-solid')
+        switchThemeBtn.classList.add('fa-regular')
+    }  else {
+        switchThemeBtn.classList.remove('fa-regular')
+        switchThemeBtn.classList.add('fa-solid')
+    }
+})
+
+burgerBtn.addEventListener('click', () => {
+    burgerBtn.classList.toggle('active')
+    burgerWindow.classList.toggle('none')
+})
+
+window.addEventListener('resize', () => {
+    if(window.innerWidth > 425) {
+        burgerBtn.classList.remove('active')
+        burgerWindow.classList.add('none')
+    }
+})
